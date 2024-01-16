909288248ebdc442ce6a288e3206e961a9647e3e
steven2308 <steven2308@outlook.com>
2024-01-16 10:24:59 -0500

    Moonriver migration script now generates tx for multisig.

--- scripts/migrations/runFinishMigrationOnMoonriver.ts
+++ scripts/migrations/runFinishMigrationOnMoonriver.ts
@@ -5,11 +5,12 @@ async function main() {
   const MigratorFactory = await ethers.getContractFactory('MoonriverMigrator');
   const migrator = MigratorFactory.attach(MOONRIVER_MIGRATOR_ADDRESS);
   const currentBatch = await migrator.currentBatch();
-  console.log(`Current batch: ${currentBatch}`);
+  const previousBatch = currentBatch.sub(1);
+  console.log(`Current batch: ${previousBatch}`);
 
-  let tx = await migrator.finishBatch(currentBatch);
+  let tx = await migrator.finishBatch(previousBatch);
   await tx.wait();
-  console.log(`Finished migrating batch ${currentBatch}`);
+  console.log(`Finished migrating batch ${previousBatch}`);
 }
 
 main().catch((error) => {
--- scripts/migrations/runStartMigrationOnMoonriver.ts
+++ scripts/migrations/runStartMigrationOnMoonriver.ts
@@ -16,19 +16,45 @@ async function main() {
   console.log(`Started a new batch`);
 
   const [holders, amounts] = await migrator.getMigrationsForBatch(currentBatch);
-  const data = {
-    holders: holders,
-    amounts: amounts.map((bn) => bn.toString()),
-    batch: currentBatch.toString(),
+  const output = {
+    version: '1.0',
+    chainId: '1284',
+    createdAt: (new Date().getTime() / 1000) | 0,
+    meta: {
+      name: 'Transactions Batch',
+      description: '',
+      txBuilderVersion: '1.16.1',
+      createdFromSafeAddress: '0x85Ab410A50A3D85f1a26d9e4eFCaa520a39B9CD6',
+      createdFromOwnerAddress: '',
+      checksum: '0x6f40d4629d1ef4bb6104abc25344f0e798e1b240e882e499a13d1046cd8efa23',
+    },
+    transactions: [
+      {
+        to: '0xbcdeFe4BeF75cf2AC1a7BBDC88bB153F1B19DB65',
+        value: '0',
+        data: null,
+        contractMethod: {
+          inputs: [
+            { internalType: 'uint256', name: 'batch', type: 'uint256' },
+            { internalType: 'address[]', name: 'tos', type: 'address[]' },
+            { internalType: 'uint256[]', name: 'amounts', type: 'uint256[]' },
+          ],
+          name: 'migrate',
+          payable: false,
+        },
+        contractInputsValues: {
+          batch: `${currentBatch}`,
+          tos: JSON.stringify(holders),
+          amounts: JSON.stringify(amounts.map((bn) => bn.toNumber())),
+        },
+      },
+    ],
   };
 
-  // Store data in a file:
   const fs = require('fs');
   const dataPath = `./migrations/${currentBatch}.json`;
-  fs.writeFileSync(dataPath, JSON.stringify(data));
-
-  console.log(`Holders: ${holders}`);
-  console.log(`Amounts: ${amounts}`);
+  fs.writeFileSync(dataPath, JSON.stringify(output));
+  console.log(`Wrote migration data to ${dataPath}`);
 }
 
 main().catch((error) => {
