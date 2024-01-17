50096d743be104c3fc63d9b7bbe3c18a763b9dc7
steven2308 <steven2308@outlook.com>
2024-01-17 15:20:21 -0500

    Updates addresses and log message.

--- scripts/runDeployMoonbeamMigrator.ts
+++ scripts/runDeployMoonbeamMigrator.ts
@@ -7,7 +7,7 @@ async function main() {
   const [deployer] = await ethers.getSigners();
   console.log('Deployer address: ', deployer.address);
 
-  console.log('Deploying RMRK token and Migrator');
+  console.log('Deploying Moonbeam Migrator');
   const legacyRMRKAddress = await getLegacyRMRKAddress();
   const MigratorFactory = await ethers.getContractFactory('Migrator');
 
